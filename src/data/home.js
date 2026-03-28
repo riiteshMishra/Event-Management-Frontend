f9d8e98fa99d8623666a82659ef5baa020f15fba
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-03-28 11:33:31 +0530

    vercel.json file added

--- src/data/home.js
+++ src/data/home.js
@@ -1,44 +1,13 @@
 export const sliderImages = [
     {
         id: 1,
-        url: "https://images.unsplash.com/photo-1506744038136-46273834b3fb",
+        url: "https://res.cloudinary.com/dwpplwqzs/image/upload/v1774677448/image_2_u9we0x.jpg",
     },
     {
         id: 2,
-        url: "https://images.unsplash.com/photo-1491553895911-0055eca6402d",
+        url: "https://res.cloudinary.com/dwpplwqzs/image/upload/v1774677222/image_1_p7mbwm.jpg",
     },
-    {
-        id: 3,
-        url: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
-    },
-    {
-        id: 4,
-        url: "https://images.unsplash.com/photo-1519389950473-47ba0277781c",
-    },
-    {
-        id: 5,
-        url: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
-    },
-    {
-        id: 6,
-        url: "https://images.unsplash.com/photo-1470770841072-f978cf4d019e",
-    },
-    {
-        id: 7,
-        url: "https://images.unsplash.com/photo-1492724441997-5dc865305da7",
-    },
-    {
-        id: 8,
-        url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
-    },
-    {
-        id: 9,
-        url: "https://images.unsplash.com/photo-1519125323398-675f0ddb6308",
-    },
-    {
-        id: 10,
-        url: "https://images.unsplash.com/photo-1469474968028-56623f02e42e",
-    },
+
 ];
 
 
