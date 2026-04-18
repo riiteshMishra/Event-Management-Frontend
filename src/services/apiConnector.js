741c5905e11bfeec258f6d99bd5f1bfa38aadf84
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-04-18 13:03:39 +0530

    env problem fixed

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -4,7 +4,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 // instence banao
 const axiosInstance = axios.create({
     baseURL: BACKEND_URL,
-    withCredentials: true
+    withCredentials: true,
 })
 
 // api connector
