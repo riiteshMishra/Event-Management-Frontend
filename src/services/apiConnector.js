81b2d3322bcc4e3757af13bb60bbfe6f59ee920d
Ritesh Mishra <riteshmishra9565@gmail.com>
2026-04-18 13:03:06 +0530

    env problem fixed

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,7 +1,5 @@
 import axios from "axios"
-
-// const BACKEND_URL = 'https://event-management-system-tyz7.onrender.com/api/v1'
-const BACKEND_URL = 'http://localhost:4000/api/v1'
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 // instence banao
 const axiosInstance = axios.create({
